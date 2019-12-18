e2845fbd2480e44c3a5d7ed5462626e3196f9af1
tomlangan <35125342+tomlangan@users.noreply.github.com>
2019-12-18 09:14:28 -0800

    Basic virtual cursor working

---
+++ Assets/Scripts/HandSimulator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSimulator : MonoBehaviour
+{
+    public GameObject VirtualHand;
+    //This is the distance the clickable plane is from the camera. Set it in the Inspector before running.
+    float DistanceZ = 0.5F;
+    Plane MousePlane;
+    public Vector3 VirtualHandPosition;
+    public Vector3 VirtualHandDirection = new Vector3(0.0F, -0.5F, 0.5F);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is how far away from the Camera the plane is placed
+        Vector3 planeOrigin = Camera.main.transform.position + (Camera.main.transform.forward * (Camera.main.nearClipPlane + DistanceZ));
+
+        MousePlane = new Plane(-Camera.main.transform.forward, planeOrigin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when there is a mouse click
+        if (Input.mousePresent)
+        {
+            //Create a ray from the Mouse click position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+
+            //Initialise the enter variable
+            float enter = 0.0f;
+
+            if (MousePlane.Raycast(ray, out enter))
+            {
+                //Get the point that is clicked
+                VirtualHandPosition = ray.GetPoint(enter);
+
+                //Move your cube GameObject to the point where you clicked
+                VirtualHand.transform.position = VirtualHandPosition;
+            }
+
+            
+            float xRelativePosFromCenter = (Input.mousePosition.x - (Screen.width / 2)) / Screen.width;
+            float zPos = Input.mousePosition.y / Screen.height;
+            float zRelativePosFromCenter = zPos - 0.5F;
+            // Black magic that needs to change if we ever change the camera position!
+            Vector3 SelectionRayTarget = new Vector3(((40.0F - (12.0F * (1.0F-zPos))) * xRelativePosFromCenter) - 2.0F , 0.0F, (21.0F * zRelativePosFromCenter) + 2.0F);
+            VirtualHandDirection = (SelectionRayTarget - VirtualHandPosition).normalized; ;
+        }
+    }
+
+    public Ray GetVirtualHandRay()
+    {
+        Ray handRay = new Ray(VirtualHandPosition, VirtualHandDirection);
+        return handRay;
+    }
+
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public HandSimulator VirtualHand;
+    public LineRenderer InputLine;
+    Plane Ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ground = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray handRay = new Ray();
+        if (null != VirtualHand)
+        {
+            handRay = VirtualHand.GetVirtualHandRay();
+        }
+
+
+        float enter;
+        if (Ground.Raycast(handRay, out enter))
+        {
+
+            //Get the point that is clicked
+            Vector3 groundIntersect = handRay.GetPoint(enter);
+
+            Vector3[] linePoints = new Vector3[2];
+            linePoints[0] = handRay.origin;
+            linePoints[1] = groundIntersect;
+            Debug.Log(" Pos[0] " + linePoints[0].ToString() + "  Pos[1] " + linePoints[1].ToString() + " Ray " + handRay.direction.ToString());
+            InputLine.SetPositions(linePoints);
+        }
+
+    }
+
+}
