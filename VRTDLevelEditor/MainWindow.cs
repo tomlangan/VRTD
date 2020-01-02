a58e690ee559170f5c45a4fb4533ab7337d9a0bb
Tom Langan <tom.langan@hotmail.com>
2020-01-02 15:49:09 -0500

    Fix up Level Editor solution

--- VRTDLevelEditor/MainWindow.cs
+++ VRTDLevelEditor/MainWindow.cs
@@ -1,44 +1,151 @@
 ﻿using System;
 using Gtk;
+using Newtonsoft.Json;
+using VRTD.Gameplay;
 
-public partial class MainWindow : Gtk.Window
+
+namespace VRTD.LevelEditor
 {
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    public class LevelDescView : Container
     {
-        SetDefaultSize(1024, 768);
-        SetPosition(WindowPosition.Center);
+        public LevelDescription LevelDesc { get; set; }
+        Label NameLabel;
 
-        DeleteEvent += delegate { Application.Quit(); };
+        public LevelDescView()
+        {
+            Table table = new Table(24, 4, true);
 
-        Fixed fix = new Fixed();
+            Add(table);
 
-        Tree levels = new Tree();
+            NameLabel = new Label();
+            table.Attach(NameLabel, 0, 4, 0, 1);
+            NameLabel.Show();
 
-        Button ExportButton = new Button("Export");
-        ExportButton.SetSizeRequest(80, 40);
+        }
+           
+        public void Refresh(LevelDescription desc)
+        {
+            NameLabel.Text = desc.Name;
 
-        ExportButton.Clicked += ExportButton_Clicked;
-
-        //fix.Put(levels, 0, 0);
-        fix.Put(ExportButton, 20, 30);
-
-        Add(fix);
-        ShowAll();
+            LevelDesc = desc;
+        }
     }
 
-    private void ExportButton_Clicked(object sender, EventArgs e)
+    public partial class MainWindow : Window
     {
-        
-    }
+        TreeView tree;
+        Table table;
+        LevelDescription LevelDesc;
+        LevelDescView LevelView;
 
-    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-    {
-        Application.Quit();
-        a.RetVal = true;
-    }
+        public MainWindow() :
+                base(WindowType.Toplevel)
+        {
+            Resize(800, 600);
+            SetPosition(WindowPosition.Center);
 
-    void LoadTestData()
-    {
+            AddWidgetsAndShow();
+
+            PopulateLevelTree();
+
+            LoadLevel("0-0");
+        }
+
+        private void AddWidgetsAndShow()
+        {
+            /* Set a handler for delete_event that immediately
+             * exits GTK. */
+            DeleteEvent += delete_event;
+
+
+            table = new Table(25, 5, true);
+
+            /* Put the table in the main window */
+            Add(table);
+
+            /* Create first button */
+            tree = new TreeView();
+
+            table.Attach(tree, 0, 1, 0, 24);
+
+            tree.Show();
+
+
+            Button exportButton = new Button("Export");
+
+            /* When the button is clicked, we call the "callback" function
+             * with a pointer to "button 2" as its argument */
+
+            exportButton.Clicked += ExportButton_Clicked;
+
+            /* Insert button 2 into the upper right quadrant of the table */
+            table.Attach(exportButton, 1, 2, 24, 25);
+
+            exportButton.Show();
+
+            /* Create "Quit" button */
+            Button quitbutton = new Button("Quit");
+
+            /* When the button is clicked, we call the "delete_event" function
+             * and the program exits */
+            quitbutton.Clicked += exit_event;
+
+            /* Insert the quit button into the both
+             * lower quadrants of the table */
+            table.Attach(quitbutton, 4, 5, 24, 25);
+
+            quitbutton.Show();
+
+            Button addLevelButton = new Button("+");
+
+            addLevelButton.Clicked += AddLevelButton_Clicked;
+            table.Attach(addLevelButton, 0, 1, 24, 25);
+            addLevelButton.Show();
+
+            LevelView = new LevelDescView();
+            table.Attach(LevelView, 1, 5, 0, 24);
+            LevelView.Show();
+
+            table.Show();
+            ShowAll();
+        }
+
+        private void PopulateLevelTree()
+        {
+        }
+
+        private void AddLevelButton_Clicked(object sender, EventArgs e)
+        {
+            AddLevelWindow dialog = new AddLevelWindow(this);
+            dialog.Show();
+        }
+
+        private void ExportButton_Clicked(object sender, EventArgs e)
+        {
+            WriteCurrentLevel();
+        }
+
+        private void LoadLevel(string levelName)
+        {
+            LevelDesc = LevelLoader.GetTestLevel();
+            LevelView.Refresh(LevelDesc);
+        }
+
+        private void WriteCurrentLevel()
+        {
+            string levelAsJSon = JsonConvert.SerializeObject(LevelDesc);
+            //StreamWriter writer = new StreamWriter();
+        }
+
+        static void delete_event(object obj, DeleteEventArgs args)
+        {
+            Application.Quit();
+        }
+
+        static void exit_event(object obj, EventArgs args)
+        {
+            Application.Quit();
+        }
 
     }
 }
