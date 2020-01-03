f6fa72095b97ee2e3cea4fc3ac1abd1ee1435878
Tom Langan <tom.langan@hotmail.com>
2020-01-02 21:20:52 -0500

    Ability to draw and resize the map

--- VRTDLevelEditor/GtkHelpers.cs
+++ VRTDLevelEditor/GtkHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Gtk;
 using System.Timers;
+using System.Collections.Generic;
 
 namespace VRTD.LevelEditor
 {
@@ -10,21 +11,25 @@ namespace VRTD.LevelEditor
         EventHandler Callback;
         object o;
         EventArgs a;
+        List<DeferredEventHelper> DeferredEventsList;
 
 
-        public DeferredEventHelper(EventHandler callback)
+        public DeferredEventHelper(EventHandler callback, List<DeferredEventHelper> list)
         {
             Callback = callback;
             t = new Timer(500);
             t.Elapsed += T_Elapsed;
+            DeferredEventsList = list;
         }
 
         private void T_Elapsed(object sender, ElapsedEventArgs e)
         {
+            t.Close();
             if (null != Callback)
             {
                 Callback(o, a);
             }
+            DeferredEventsList.Remove(this);
         }
 
         public void Fired(object sender, EventArgs args)
@@ -34,10 +39,30 @@ namespace VRTD.LevelEditor
             t.Stop();
             t.Start();
         }
+
+        public void Flush()
+        {
+            t.Stop();
+            t.Close();
+            if (null != Callback)
+            {
+                Callback(o, a);
+            }
+            DeferredEventsList.Remove(this);
+        }
     }
 
     public class GtkHelpers
     {
+        public static List<DeferredEventHelper> DeferredEvents = new List<DeferredEventHelper>();
+
+        public static void FlushAllDeferredEvents()
+        {
+            for (int i = DeferredEvents.Count; i > 0; i--)
+            {
+                DeferredEvents[i - 1].Flush();
+            }
+        }
 
         public static HBox TextEntryField(string fieldName, string value, EventHandler callback, bool deferred = false)
         {
@@ -53,7 +78,7 @@ namespace VRTD.LevelEditor
             entry.Text = value;
             if (deferred)
             {
-                DeferredEventHelper h = new DeferredEventHelper(callback);
+                DeferredEventHelper h = new DeferredEventHelper(callback, DeferredEvents);
                 entry.Changed += h.Fired;
             }
             else
@@ -61,6 +86,33 @@ namespace VRTD.LevelEditor
                 entry.Changed += callback;
             }
             entry.Show();
+
+            return box;
+        }
+
+
+        public static HBox ComboBox(string fieldName, string[] values, int initialValueIndex, EventHandler callback, bool deferred = false)
+        {
+            HBox box = new HBox(false, 20);
+
+            Label label = new Label(fieldName);
+            box.Add(label);
+            label.Show();
+
+            ComboBox dropdown = new ComboBox(values);
+            dropdown.WidthRequest = 200;
+            dropdown.Active = initialValueIndex;
+            box.Add(dropdown);
+            if (deferred)
+            {
+                DeferredEventHelper h = new DeferredEventHelper(callback, DeferredEvents);
+                dropdown.Changed += h.Fired;
+            }
+            else
+            {
+                dropdown.Changed += callback;
+            }
+            dropdown.Show();
 
             return box;
         }
--- VRTDLevelEditor/LevelEditLayout.cs
+++ VRTDLevelEditor/LevelEditLayout.cs
@@ -6,8 +6,10 @@ namespace VRTD.LevelEditor
 {
     public class LevelEditLayout : Layout
     {
+        static string[] LayoutOptions = { "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60", "65", "70", "75", "80", "85", "90", "100" };
+
         public LevelDescription LevelDesc { get; set; }
-        VBox Layout;
+        VBox Layout = null;
         public delegate void TreeRefreshNeededFunc();
         public event TreeRefreshNeededFunc TreeRefreshNeeded;
 
@@ -19,6 +21,15 @@ namespace VRTD.LevelEditor
 
         public void SetLevel(LevelDescription desc)
         {
+            GtkHelpers.FlushAllDeferredEvents();
+
+            if (null != Layout)
+            {
+                Layout.HideAll();
+                Layout.Destroy();
+                Layout = null;
+            }
+
             Layout = new VBox(false, 20);
             Add(Layout);
 
@@ -26,14 +37,61 @@ namespace VRTD.LevelEditor
             Layout.Add(field);
             field.Show();
 
-            
+            field = GtkHelpers.ComboBox("Width", LayoutOptions, (desc.FieldWidth/5 - 1), Width_Changed, true);
+            Layout.Add(field);
+            field.Show();
+
+            field = GtkHelpers.ComboBox("Depth", LayoutOptions, (desc.FieldDepth / 5 - 1), Depth_Changed, true);
+            Layout.Add(field);
+            field.Show();
+
+            Table map = GetFieldTable(desc);
+            Layout.Add(map);
+            map.Show();
 
             Layout.ShowAll();
-
             LevelDesc = desc;
         }
 
-        
+        private Table GetFieldTable(LevelDescription desc)
+        {
+            Table t = new Table((uint)desc.FieldDepth, (uint)desc.FieldWidth, true);
+
+            for (uint i = 0; i < desc.Map.Count; i++)
+            {
+                Button b = new Button();
+                SetFieldButtonType(b, desc.Map[(int)i]);
+                uint xpos = i % (uint)desc.FieldWidth;
+                uint ypos = i / (uint)desc.FieldWidth;
+                t.Attach(b, xpos, xpos + 1, ypos, ypos + 1);
+                b.Show();
+            }
+
+            return t;
+        }
+
+        private void SetFieldButtonType(Button b, char c)
+        { 
+            Gdk.Color col = new Gdk.Color();
+            string s = "";
+            switch (c)
+            {
+                case 'D':
+                    Gdk.Color.Parse("green", ref col);
+                    s = "D";
+                    break;
+                case 'R':
+                    Gdk.Color.Parse("black", ref col);
+                    s = "R";
+                    break;
+                case 'T':
+                    Gdk.Color.Parse("grey", ref col);
+                    s = "T";
+                    break;
+            }
+            b.ModifyBg(StateType.Normal, col);
+            b.Label = s;
+        }
 
         private void Name_Changed(object sender, EventArgs e)
         { 
@@ -48,6 +106,57 @@ namespace VRTD.LevelEditor
                     TreeRefreshNeeded();
                 }
             }
+        }
+
+
+        private void Width_Changed(object sender, EventArgs e)
+        {
+            int newIndex = ((ComboBox)sender).Active;
+            int newValue = int.Parse(LayoutOptions[newIndex]);
+            if (LevelDesc.FieldWidth != newValue)
+            {
+                LevelDesc.FieldWidth = newValue;
+                WriteChanges();
+            }
+
+            // Need to redraw the field
+            int newFieldSize = LevelDesc.FieldWidth * LevelDesc.FieldDepth;
+            ResizeMap(newFieldSize);
+        }
+
+
+        private void Depth_Changed(object sender, EventArgs e)
+        {
+            int newIndex = ((ComboBox)sender).Active;
+            int newValue = int.Parse(LayoutOptions[newIndex]);
+            if (LevelDesc.FieldDepth != newValue)
+            {
+                LevelDesc.FieldDepth = newValue;
+                WriteChanges();
+            }
+
+
+            // Need to redraw the field
+            int newFieldSize = LevelDesc.FieldWidth * LevelDesc.FieldDepth;
+            ResizeMap(newFieldSize);
+        }
+
+
+        private void ResizeMap(int newFieldSize)
+        {
+            if (LevelDesc.Map.Count > newFieldSize)
+            {
+                LevelDesc.Map.RemoveRange(newFieldSize, LevelDesc.Map.Count - newFieldSize);
+            }
+            else if (LevelDesc.Map.Count < newFieldSize)
+            {
+                int itemsToAdd = newFieldSize - LevelDesc.Map.Count;
+                for (int i = 0; i < itemsToAdd; i++)
+                {
+                    LevelDesc.Map.Add('D');
+                }
+            }
+            SetLevel(LevelDesc);
         }
 
         void WriteChanges()
